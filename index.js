88dc7f2cddc6431b6813c0a3646890489a52d38b
Ayoub Douiri <ayoubdouiri65@gmail.com>
2023-07-03 14:45:08 +0100

    Enter login details and password verification

--- index.js
+++ index.js
@@ -1,4 +1,31 @@
 let username; // the person that logs in
-let password; // the password that is entered
 
 
+username=prompt("What is your username?","");
+username=username.trim();
+username=username.toLowerCase();
+
+if(username=="admin"){
+	askForPassword();
+}
+else{
+	alert("Failed Login Credentials!");
+}
+
+
+
+function askForPassword(){
+	/*Verifies the user entered password, returns null and takes no parameters */
+
+	let password; // the password that is entered
+	password=prompt("Enter your password","");
+	if(password=="TheMaster"){
+		alert("WELCOME!");
+	}
+	else if(password.length==0){
+		alert("CANCELLED!");
+	}
+	else{
+		alert("Wrong Password!");
+	}
+}
